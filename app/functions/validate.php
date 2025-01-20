9e785d7e21e7d74d970085c103774ddc3516101f
Douglas Almeida <110480637+douglasalmeida21091994@users.noreply.github.com>
2025-01-19 23:04:31 -0300

    Update: ajuste no validate

--- app/functions/validate.php
+++ app/functions/validate.php
@@ -1,44 +1,24 @@
 <?php
     function validate(array $fields) {
         foreach($fields as $field => $type) {
+           $validate = [];
+
            switch($type) {
                case 's':
-                   if (!isset($_POST[$field]) || strlen($_POST[$field]) == 0) {
-                       return false;
-                   }
+                   $validate[$field] = filter_input(INPUT_POST, $field, FILTER_SANITIZE_STRING);
                    break;
                case 'e':
-                   if (!filter_var($_POST[$field], FILTER_VALIDATE_EMAIL)) {
-                       return false;
-                   }
+                   $validate[$field] = filter_input(INPUT_POST, $field, FILTER_SANITIZE_EMAIL);
                    break;
                case 'i':
-                   if (!filter_var($_POST[$field], FILTER_VALIDATE_INT)) {
-                       return false;
-                   }
-                   break;
-               case 'b':
-                   if (!filter_var($_POST[$field], FILTER_VALIDATE_BOOLEAN)) {
-                       return false;
-                   }
+                   $validate[$field] = filter_input(INPUT_POST, $field, FILTER_SANITIZE_NUMBER_INT);
                    break;
                case 'f':
-                   if (!filter_var($_POST[$field], FILTER_VALIDATE_FLOAT)) {
-                       return false;
-                   }
-                   break;
-               case 'url':
-                   if (!filter_var($_POST[$field], FILTER_VALIDATE_URL)) {
-                       return false;
-                   }
-                   break;
-               case 'ip':
-                   if (!filter_var($_POST[$field], FILTER_VALIDATE_IP)) {
-                       return false;
-                   }
+                   $validate[$field] = filter_input(INPUT_POST, $field, FILTER_SANITIZE_NUMBER_FLOAT);
                    break;
                default:
-                   return false;
+                   $validate[$field] = filter_input(INPUT_POST, $field, FILTER_SANITIZE_STRING);
+                   break;
            }
         }
     }
